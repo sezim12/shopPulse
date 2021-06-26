5e61de935d59554026f6e11e15932fbd061d38d8
sezim12 <vip.ssosezim825@gmail.com>
2021-06-26 18:20:56 +0600

    full project

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,72 @@ $(document).ready(function () {
     });
 
 });
+
+
+//tabs
+$('div.catalog__buttons').on('click', 'button:not(.catalog__btn-active)', function () {
+    $(this)
+        .addClass('catalog__btn-active').siblings().removeClass('catalog__btn-active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content-active')
+        .eq($(this).index()).addClass('catalog__content-active');
+});
+
+//more btn
+$('.catalog__content-card-moreBtn').each(function (i) {
+    $(this).on('click' , function (e) {
+        e.preventDefault();
+        $('.catalog__content-card-item').eq(i).addClass( 'catalog__content-card-item-active');
+        $('.catalog__content-card-about').eq(i).addClass( 'catalog__content-card-about-active')
+
+    })
+});
+//back btn
+$('.catalog__content-card-backBtn').each(function (i) {
+$(this).on('click' , function (e) {
+        e.preventDefault();
+        $('.catalog__content-card-item').eq(i).removeClass( 'catalog__content-card-item-active');
+        $('.catalog__content-card-about').eq(i).removeClass( 'catalog__content-card-about-active')
+
+    })
+});
+// up-btn
+$(window).scroll(function () {
+    if($(this).scrollTop() > 500){
+        $('.up-arrow').fadeIn();
+    } else {
+        $('.up-arrow').fadeOut();
+    }
+
+});
+// overlay
+$('.header__contact-button, .hero__content-button').on('click', function () {
+    $('.overlay, .popup, .consultation').fadeIn('slow');
+
+});
+
+
+$('.popup__close').on('click', function () {
+    $('.overlay, .popup, .consultation, .thanks, .buy-form').fadeOut('slow')
+
+});
+
+
+
+
+
+
+$('.consult__form-btn').on('click', function () {
+    $('.overlay, .popup, .consultation').fadeIn('slow');
+    // $('.consultation').fadeOut();
+});
+
+$('.catalog__content-card-footer-btn').each(function (i) {
+    $(this).on('click', function () {
+        $('.overlay, .popup, .buy-form').fadeIn('slow');
+    })
+});
+
+
+
+
+
